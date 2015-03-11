e645e6fe98fbcd5d0ef01e442d6be05d82c01fc2
CDillinger <collin.dillinger@gmail.com>
2015-03-11 19:23:00 -0400

    Initial code commit

---
+++ ParcelExtractor/ParcelExtractor/Envelope.cs
@@ -0,0 +1,122 @@
+﻿/*
+
+ *  ParcelExtractor - Ingham County Parcel Data Extraction
+ *  Copyright (C) 2015  Collin Dillinger
+
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ */
+using System.Xml.Serialization;
+
+namespace ParcelExtractor
+{
+	[XmlType(AnonymousType = true, Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+	[XmlRoot("Envelope", Namespace = "http://schemas.xmlsoap.org/soap/envelope/", IsNullable = false)]
+	public class Envelope
+	{
+		[XmlElement("Body")]
+		public Body Body { get; set; }
+	}
+
+	[XmlType(AnonymousType = true, Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+	public class Body
+	{
+		[XmlElement("GetDataResponse", Namespace = "http://tempuri.org/")]
+		public DataResponse Response { get; set; }
+	}
+
+	[XmlType(AnonymousType = true, Namespace = "http://tempuri.org/")]
+	[XmlRoot("GetDataResponse", Namespace = "http://tempuri.org/", IsNullable = false)]
+	public class DataResponse
+	{
+		[XmlArray("GetDataResult")]
+		[XmlArrayItem("clsParcel", Namespace = "http://schemas.datacontract.org/2004/07/ParcelServiceApp", IsNullable = false)]
+		public Parcel[] Results { get; set; }
+	}
+
+	[XmlType(AnonymousType = true, Namespace = "http://schemas.datacontract.org/2004/07/ParcelServiceApp")]
+	[XmlRoot("CLSParcel", Namespace = "http://schemas.datacontract.org/2004/07/ParcelServiceApp", IsNullable = false)]
+	public class Parcel
+	{
+		[XmlElement("Acreage")]
+		public double Acreage { get; set; }
+
+		[XmlElement("LastName")]
+		public string LastName { get; set; }
+
+		[XmlElement("Owner")]
+		public string Owner { get; set; }
+
+		[XmlElement("OwnerAddress")]
+		public string OwnerAddress { get; set; }
+
+		[XmlElement("OwnerCity")]
+		public string OwnerCity { get; set; }
+
+		[XmlElement("OwnerState")]
+		public string OwnerState { get; set; }
+
+		[XmlElement("OwnerZIP")]
+		public string OwnerZIP { get; set; }
+
+		[XmlElement("PIN")]
+		public string PIN { get; set; }
+
+		[XmlElement("Parcel")]
+		public string ParcelNumber { get; set; }
+
+		[XmlElement("Pre")]
+		public string Pre { get; set; }
+
+		[XmlElement("PropertyApartment")]
+		public string PropertyApartment { get; set; }
+
+		[XmlElement("PropertyCity")]
+		public string PropertyCity { get; set; }
+
+		[XmlElement("PropertyClass")]
+		public uint PropertyClass { get; set; } // TODO: get enum values
+
+		[XmlElement("PropertyDirection")]
+		public string PropertyDirection { get; set; }
+
+		[XmlElement("PropertyNumber")]
+		public string PropertyNumber { get; set; }
+
+		[XmlElement("PropertyStreet")]
+		public string PropertyStreet { get; set; }
+
+		[XmlElement("PropertyZIP")]
+		public string PropertyZIP { get; set; }
+
+		[XmlElement("SchoolDistrict")]
+		public uint SchoolDistrict { get; set; } // TODO: get enum values
+
+		[XmlElement("Sev2014")]
+		public double StateEqualizedValue2014 { get; set; }
+
+		[XmlElement("Tax2014")]
+		public double TaxableValue2014 { get; set; }
+
+		[XmlElement("center")]
+		public string Center { get; set; }
+
+		[XmlElement("geo")]
+		public string Geo { get; set; }
+
+		[XmlElement("geom")]
+		public string Geom { get; set; }
+	}
+}
---
+++ ParcelExtractor/ParcelExtractor/WebConnect.cs
@@ -0,0 +1,79 @@
+﻿/*
+
+ *  ParcelExtractor - Ingham County Parcel Data Extraction
+ *  Copyright (C) 2015  Collin Dillinger
+
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ */
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ParcelExtractor
+{
+	public class WebConnect
+	{
+		private readonly HttpClient _client;
+
+		public WebConnect()
+		{
+			_client = new HttpClient();
+			_client.DefaultRequestHeaders.Add("soapaction", "http://tempuri.org/IParcelServiceApp/GetData");
+		}
+
+		public enum SearchType
+		{
+			Address,
+			LastName,
+			ParcelNumber
+		}
+
+		private static string ComposeRequestString(SearchType searchType, string query)
+		{
+			var typeString = "";
+			switch (searchType)
+			{
+				case SearchType.Address:
+					typeString = "AddrQuery2";
+					break;
+				case SearchType.ParcelNumber:
+					typeString = "PinQuery2";
+					break;
+				case SearchType.LastName:
+					typeString = "OwnerQuery2";
+					break;
+			}
+
+			return string.Format("<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\"><s:Body><GetData xmlns=\"http://tempuri.org/\"><spName>{0}</spName><queryValue>{1}%</queryValue></GetData></s:Body></s:Envelope>", typeString, query);
+		}
+
+		public async Task<Envelope> QueryAsync(SearchType searchType, string query = "", string query2 = "")
+		{
+			if (searchType == SearchType.Address)
+				query = string.Format("{0},{1}", query2, query);
+
+			// Query the service
+			var response = await _client.PostAsync("http://ingham-equalization.rsgis.msu.edu/ParcelServiceApp/ParcelServiceApp.svc", new StringContent(ComposeRequestString(searchType, query), Encoding.UTF8, "text/xml"));
+			if (!response.IsSuccessStatusCode)
+				throw new ConnectionFailedException();
+
+			var stream = await response.Content.ReadAsStreamAsync();
+			var serializer = new XmlSerializer(typeof(Envelope));
+			return (Envelope)serializer.Deserialize(stream);
+		}
+	}
+}
